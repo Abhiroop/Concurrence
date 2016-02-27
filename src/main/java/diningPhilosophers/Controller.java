a6d9cf10f4af92910cd5f0e5f4ba387d6784cfb9
Abhiroop Sarkar <asiamgenius@gmail.com>
2016-02-28 03:08:49 +0530

    Modelling the erratic behaviour without sysnchronization

--- src/main/java/diningPhilosophers/Controller.java
+++ src/main/java/diningPhilosophers/Controller.java
@@ -1,5 +1,34 @@
 package diningPhilosophers;
 
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import com.google.common.collect.Lists;
+
 public class Controller {
+
+	public static final int NO_OF_PHILOSOPHERS = 5;
+
+	public static void main(String args []){
+
+		List<Thread> philosopherList=Lists.newArrayList();
+
+		final CountDownLatch countDownLatch = new CountDownLatch(NO_OF_PHILOSOPHERS);
+
+		for(int i=0;i<NO_OF_PHILOSOPHERS;i++){
+			Philosopher philosopher = new Philosopher(i,countDownLatch);
+			Thread thread = new Thread(philosopher);
+			philosopherList.add(thread);
+		}
+
+		philosopherList.forEach(Thread::start);
+
+		try{
+			countDownLatch.await();  
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+
+	}
 
 }
--- src/main/java/diningPhilosophers/Philosopher.java
+++ src/main/java/diningPhilosophers/Philosopher.java
@@ -1,5 +1,70 @@
 package diningPhilosophers;
 
-public class Philosopher {
+import java.util.concurrent.CountDownLatch;
+
+public class Philosopher implements Runnable {
+	
+	boolean leftFork;
+	
+	boolean rightFork;
+	
+	int i;
+	
+	private final CountDownLatch countDownLatch;
+	
+	public Philosopher(int i, CountDownLatch countDownLatch){
+		this.i=i;
+		this.leftFork = false;
+		this.rightFork = false;
+		this.countDownLatch = countDownLatch;
+		
+	}
+	
+	public void startEating() throws InterruptedException{
+		System.out.println(i + " - Started Eating");
+		Thread.sleep(5000);
+		System.out.println(i + " - Finished Eating");
+	}
+	
+	public void startThinking() throws InterruptedException{
+		System.out.println(i + " - Started Thinking");
+		Thread.sleep(5000);
+		System.out.println(i + " - Finished Thinking");
+	}
+	
+	public boolean isLeftFork() {
+		return leftFork;
+	}
+
+	public void setLeftFork(boolean leftFork) {
+		this.leftFork = leftFork;
+	}
+
+	public boolean isRightFork() {
+		return rightFork;
+	}
+
+	public void setRightFork(boolean rightFork) {
+		this.rightFork = rightFork;
+	}
+
+	public void run() {
+		countDownLatch.countDown();
+		while(true){
+			try {
+				startEating();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				startThinking();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
 
 }
