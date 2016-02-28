d6a5ae316c2f1efe6c4e2af2ccc496cee499e1d6
Abhiroop Sarkar <asiamgenius@gmail.com>
2016-02-28 14:22:30 +0530

    Added resources which induce contention

--- src/main/java/diningPhilosophers/Controller.java
+++ src/main/java/diningPhilosophers/Controller.java
@@ -1,5 +1,6 @@
 package diningPhilosophers;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
@@ -9,14 +10,18 @@ public class Controller {
 
 	public static final int NO_OF_PHILOSOPHERS = 5;
 
-	public static void main(String args []){
+	public static void main(String args[]){
 
 		List<Thread> philosopherList=Lists.newArrayList();
+		
+		boolean[] forks = new boolean[NO_OF_PHILOSOPHERS];
+		
+		Arrays.fill(forks, true);
 
 		final CountDownLatch countDownLatch = new CountDownLatch(NO_OF_PHILOSOPHERS);
 
 		for(int i=0;i<NO_OF_PHILOSOPHERS;i++){
-			Philosopher philosopher = new Philosopher(i,countDownLatch);
+			Philosopher philosopher = new Philosopher(i,countDownLatch, forks);
 			Thread thread = new Thread(philosopher);
 			philosopherList.add(thread);
 		}
--- src/main/java/diningPhilosophers/Philosopher.java
+++ src/main/java/diningPhilosophers/Philosopher.java
@@ -8,7 +8,7 @@ public class Philosopher implements Runnable {
 	
 	boolean rightFork;
 	
-	int i;
+	int id;
 	
 	int eatCounter;
 	
@@ -16,32 +16,73 @@ public class Philosopher implements Runnable {
 	
 	private final CountDownLatch countDownLatch;
 	
-	public Philosopher(int i, CountDownLatch countDownLatch){
-		this.i=i;
+	private boolean forks[];
+	
+	public Philosopher(int id, CountDownLatch countDownLatch, boolean forks[]){
+		this.id=id;
 		this.leftFork = false;
 		this.rightFork = false;
 		this.countDownLatch = countDownLatch;
 		this.eatCounter = 0;
 		this.thinkCounter = 0;
+		this.forks=forks;
 		
 	}
 	
 	public void startEating() throws InterruptedException{
 		if(leftFork && rightFork){
-			System.out.println(i + " - Started Eating");
+			pickupFork();
+			System.out.println(id + " - Started Eating");
 			Thread.sleep(5000);
 			this.eatCounter++;
-			System.out.println(i + " - Finished Eating " + this.eatCounter + "times.");
-			setLeftFork(false);
-			setRightFork(false);
+			System.out.println(id + " - Finished Eating " + this.eatCounter + " times.");
+			dropFork();
+		}else{
+			dropFork();
+			startThinking();
 		}
 	}
 	
+	private void dropFork() {
+		if(!forks[id]){
+			leftFork = false;
+			forks[id] = true;
+		}
+		
+		if(id<forks.length - 1 && !forks[id+1]){
+			rightFork = false;
+			forks[id+1] = true;
+		}
+		
+		if(id==forks.length-1 && !forks[0]){
+			rightFork = false;
+			forks[0] = true;
+		}
+	}
+
+	private void pickupFork() {
+		
+		if(forks[id]){
+			leftFork = true;
+			forks[id] = false;
+		}
+		
+		if(id<forks.length - 1 && forks[id+1]){
+			rightFork = true;
+			forks[id+1] = false;
+		}
+		
+		if(id==forks.length-1 && forks[0]){
+			rightFork = true;
+			forks[0] = false;
+		}
+	}
+
 	public void startThinking() throws InterruptedException{
-		System.out.println(i + " - Started Thinking");
+		System.out.println(id + " - Started Thinking");
 		Thread.sleep(5000);
 		this.thinkCounter++;
-		System.out.println(i + " - Finished Thinking " + this.thinkCounter + "times.");
+		System.out.println(id + " - Finished Thinking " + this.thinkCounter + " times.");
 	}
 	
 	public boolean isLeftFork() {
@@ -66,13 +107,6 @@ public class Philosopher implements Runnable {
 			try {
 				startEating();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				startThinking();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
